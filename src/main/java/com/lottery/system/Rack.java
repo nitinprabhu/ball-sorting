9153921fb8f96b852029a9a68cb923eb051c7b03
Nitin Prabhu <prabhunitin61@gmail.com>
2017-01-17 21:40:22 +0000

    Added more test cases to sort multiple input values

--- src/main/java/com/lottery/system/Rack.java
+++ src/main/java/com/lottery/system/Rack.java
@@ -5,20 +5,27 @@ public class Rack {
 	
 	public static int[] getNumbersDrawn(int inputArray[]) {
 		
-	if(inputArray.length <=1){
+	if(inputArray.length <=1)
+	{
 			return inputArray;
-		}
-		else
-		{
-		 if(inputArray[0] > inputArray[1]) 
-		 {
-				int temp=inputArray[0];
-				inputArray[0]=inputArray[1];
-				inputArray[1]=temp;
-			}
-			
-		}
-		 return inputArray;
+	}
+	else
+	{
+		 for (int i = 0; i < inputArray.length; i++) {
+		        for (int j = 1; j < (inputArray.length - i); j++) {
+
+		            if (inputArray[j - 1] > inputArray[j]) {
+		                int temp = inputArray[j - 1];
+		                inputArray[j - 1] = inputArray[j];
+		                inputArray[j] = temp;
+		            }
+
+		        }
+		    }
+		
+	}
+	
+	return inputArray;
 		
 		
 	}
--- src/test/java/com/lottery/system/test/WinningNumberSortTest.java
+++ src/test/java/com/lottery/system/test/WinningNumberSortTest.java
@@ -25,4 +25,21 @@ public class WinningNumberSortTest {
 		
 		Assert.assertArrayEquals(new int[]{20,30}, Rack.getNumbersDrawn(new int[]{30,20}));
 	}
+	
+	@Test
+	public void test_sorting_when_five_winning_number_are_generated()
+	{
+		
+		Assert.assertArrayEquals(new int[]{5,34,38,44,58}, Rack.getNumbersDrawn(new int[]{58,5,44,38,34}));
+	}
+	
+	@Test
+	public void test_sorting_when_seven_winning_number_are_generated()
+	{
+		
+		Assert.assertArrayEquals(new int[]{1,5,15,29,41,57,59}, Rack.getNumbersDrawn(new int[]{57,5,1,59,41,15,29}));
+	}
+	
+	
+	
 }
